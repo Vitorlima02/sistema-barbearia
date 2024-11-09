8b24f5f3201acd074be34fc8d1d7aaac50a2a728
Vitor Lima <vitorliman@gmail.com>
2024-11-09 11:43:38 -0300

    feat: add Employee classe

---
+++ src/Employee.java
@@ -0,0 +1,29 @@
+public class Employee extends Person {
+    private int id;
+    private double salary;
+    private String position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
