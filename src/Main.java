9f8474ee7ae1fc1ef9eb774ea52d87a2b73880c7
Vitor Lima <vitorliman@gmail.com>
2024-11-10 10:26:55 -0300

    feat: add inicio do Menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        while (true) {
+            showMenu();
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    break;
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    return;
+            }
+        }
+
+
+    }
+
+    public static void showMenu() {
+        System.out.println("Sistema de Agendamento Barbearia");
+        System.out.println("=============================");
+        System.out.println("--1: Cadastrar Cliente");
+        System.out.println("--0: Sair");
+        System.out.println("Informe a opção desejada: ");
     }
 }
